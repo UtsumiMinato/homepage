14a6757e4440659cefac93911317544c8ad39f42
Utsumiminato <newxmith@gmail.com>
2022-08-01 00:13:24 +0800

    add search function

--- script/script.js
+++ script/script.js
@@ -15,3 +15,37 @@ function startTime(){
     }
     return i;
   }
+
+
+
+  let searchBar = document.querySelector('#search')//文字框
+
+  searchBar.addEventListener('keydown',function(e){
+    if(e.code == 'Enter'){
+      sendWords()
+    }
+  })//如果按下Enter就執行sendWords funsction
+
+
+  function sendWords(){
+    if (searchBar.value.indexOf("www.") != -1) {
+      let searchUrl = "https://" + searchBar.value
+      document.location.href=searchUrl;
+      searchBar.value = ''
+      return false
+    }
+    else if(searchBar.value.indexOf(".") != -1){
+      let searchUrl = "https://www." + searchBar.value
+      document.location.href=searchUrl;
+      searchBar.value = ''
+      return false
+    }
+    else{
+      let searchUrl = "https://www.google.com/search?q=" + searchBar.value
+      document.location.href=searchUrl;
+      searchBar.value = ''
+      return false
+    }
+    
+    
+  }
